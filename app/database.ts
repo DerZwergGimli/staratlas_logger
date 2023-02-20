9e734faea5779b127a4e95046538f22511dff36b
DZWG <paule22@protonmail.com>
2023-02-20 20:24:23 +0100

    added mongo url as env

--- app/database.ts
+++ app/database.ts
@@ -1,21 +1,25 @@
 import mongoose from 'mongoose';
+import * as process from 'process';
 
 let database: mongoose.Connection;
 export const connectDB = () => {
-  // add your own uri below
-  const uri =
-    'mongodb+srv://writer:wr7Dtq13MafK4LTe@cluster0.2hmgfgu.mongodb.net/?retryWrites=true&w=majority';
-  if (database) {
-    return;
+  if (process.env.MONGOURL) {
+    // add your own uri below
+    const uri = process.env.MONGOURL;
+    if (database) {
+      return;
+    }
+    mongoose.connect(uri);
+    database = mongoose.connection;
+    database.once('open', async () => {
+      console.log('Connected to database');
+    });
+    database.on('error', () => {
+      console.log('Error connecting to database');
+    });
+  } else {
+    console.log('Please set the ENV:(MONGOURL)');
   }
-  mongoose.connect(uri);
-  database = mongoose.connection;
-  database.once('open', async () => {
-    console.log('Connected to database');
-  });
-  database.on('error', () => {
-    console.log('Error connecting to database');
-  });
 };
 export const disconnectDB = () => {
   if (!database) {
